fcf3caa03adb32ea6dbd02196b8be8570210752b
Meisuleen Kased <meisuleen@gmail.com>
2018-11-08 18:31:29 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,6 @@ def oxford_comma(array)
   elsif array.size == 2
      array.join(" and ")
   else
-    array[-2..-1].join(", and ") + array[-1]
+    array[0..-1].join(", and ") + array[-1]
   end
 end
