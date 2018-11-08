ef4be706d5c99f331f4d6e23e268c1a627a737cf
Meisuleen Kased <meisuleen@gmail.com>
2018-11-08 18:22:16 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,12 @@
+require 'pry'
 def oxford_comma(array)
   if array.size == 1
     array.join
   elsif array.size == 2
      array.join(" and ")
   else
+    binding.pry
   array[-1].join(", and ")
+  
   end
 end
